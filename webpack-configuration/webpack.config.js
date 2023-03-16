12a1be18364d09bb1a3b267699adeb281f98c85a
Alish <maratov.alr@gmail.com>
2023-03-17 01:41:01 +0600

    changing webpack

--- webpack-configuration/webpack.config.js
+++ webpack-configuration/webpack.config.js
@@ -1,14 +1,45 @@
+// const webpack = require("webpack");
+// const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// module.exports = {
+//   mode: "development",
+//   entry: '../todolist/src/todo.ts',
+//   module: {
+//     rules:[
+//         {
+//             test:/\.ts$/,
+//             use:'ts-loader',
+//             exclude:/node_modules/
+//         }
+//     ]
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "../dist"),
+//     filename: "webpack-output.js",
+//   },
+//   plugins:[new HtmlWebpackPlugin()]
+
+// }
+
 const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: '../todolist/src/todo.js',
+  entry: '../todolist/src/todo.ts',
+  module: {
+        rules:[
+            {
+                test:/\.ts$/,
+                use:'ts-loader',
+                exclude:/node_modules/
+            }
+        ]
+      },
   output: {
     filename: "webpack-output.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    path: path.resolve(__dirname, "../todolist/src/dist"),
   }
 }
