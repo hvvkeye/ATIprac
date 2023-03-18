acef5c88ecc0bbda4b159b31b8bf012b4b3ae9f7
Alish <maratov.alr@gmail.com>
2023-03-18 18:39:58 +0600

    changing the webpack conf

--- webpack-configuration/webpack.config.js
+++ webpack-configuration/webpack.config.js
@@ -1,45 +1,28 @@
-// const webpack = require("webpack");
-// const path = require("path");
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-// module.exports = {
-//   mode: "development",
-//   entry: '../todolist/src/todo.ts',
-//   module: {
-//     rules:[
-//         {
-//             test:/\.ts$/,
-//             use:'ts-loader',
-//             exclude:/node_modules/
-//         }
-//     ]
-//   },
-//   output: {
-//     path: path.resolve(__dirname, "../dist"),
-//     filename: "webpack-output.js",
-//   },
-//   plugins:[new HtmlWebpackPlugin()]
-
-// }
-
 const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: "development",
   entry: '../todolist/src/todo.ts',
   module: {
-        rules:[
-            {
-                test:/\.ts$/,
-                use:'ts-loader',
-                exclude:/node_modules/
-            }
-        ]
-      },
+    rules:[
+        {
+            test:/\.ts$/,
+            use:'ts-loader',
+            exclude:/node_modules/
+        }
+    ]
+  },
   output: {
     filename: "webpack-output.js",
-    path: path.resolve(__dirname, "../todolist/src/dist"),
-  }
+    path : path.resolve(__dirname,'../todolist/')
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'../todolist/index.html',
+      inject:'body'
+    })  
+  ]
 }
